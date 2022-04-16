a51ceb6fa9ee68e66b2fb8e77e3e11c3458e28cd
emeraldhycient <igwezehycient86@gmail.com>
2022-04-16 14:31:08 +0100

    reduced tools

--- components/Tools.jsx
+++ components/Tools.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function Tools() {
   return (
     <section className="h-full pb-6 w-full bg-slate-900">
-      <div className="mt-20 md:mt-12 items-center">
-        <div className="w-11/12 mx-auto">
+      <div className="mt-20 md:mt-12 items-center w-full">
+        <div className="w-full md:w-11/12 mx-auto">
           <div className="w-full flex justify-center items-center">
             <h1 className="text-5xl font-extrabold text-white tracking-wide leading-tight mt-20 mb-12 ">
               Tools.
@@ -17,38 +17,22 @@ function Tools() {
             <i className="si si-react si--color text-6xl mb-6"></i>
             <i className="si si-vite si--color text-6xl mb-6"></i>
             <i className="si si-tailwindcss si--color text-6xl mb-6"></i>
-            <i className="si si-materialui si--color text-6xl mb-6"></i>
             <i className="si si-bootstrap si--color text-6xl mb-6"></i>
-            <i className="si si-fontawesome si--color text-6xl mb-6"></i>
             <i className="si si-firebase si--color text-6xl mb-6"></i>
-            <i className="si si-visualstudiocode si--color text-6xl mb-6"></i>
-
             <i className="si si-nextdotjs text-white text-6xl mb-6"></i>
             <i className="si si-netlify si--color text-6xl mb-6"></i>
             <i className="si si-vercel text-white text-6xl mb-6"></i>
-            <i className="si si-figma si--color text-6xl mb-6"></i>
-            <i className="si si-wordpress si--color text-6xl mb-6"></i>
-            <i className="si si-woocommerce si--color text-6xl mb-6"></i>
-            <i className="si si-stripe si--color text-6xl mb-6"></i>
-            <i className="si si-electron si--color text-6xl mb-6"></i>
             <i className="si si-npm si--color text-6xl mb-6"></i>
             <i className="si si-yarn si--color text-6xl mb-6"></i>
-            <i className="si si-babel si--color text-6xl mb-6"></i>
-            <i className="si si-webpack si--color text-6xl mb-6"></i>
             <i className="si si-laravel si--color text-6xl mb-6"></i>
             <i className="si si-php si--color text-6xl mb-6"></i>
             <i className="si si-phpmyadmin si--color text-6xl mb-6"></i>
-            <i className="si si-sqlite si--color text-6xl mb-6"></i>
             <i className="si si-android si--color text-6xl mb-6"></i>
             <i className="si si-apple si--color bg-white  rounded text-center text-5xl mb-6"></i>
             <i className="si si-nodedotjs si--color text-6xl mb-6"></i>
             <i className="si si-chartdotjs si--color text-6xl mb-6"></i>
             <i className="si si-git si--color text-6xl mb-6"></i>
             <i className="si si-github text-white text-6xl mb-6"></i>
-            <i className="si si-jquery text-white text-6xl mb-6"></i>
-            <i className="si si-linux text-white text-6xl mb-6"></i>
-            <i className="si si-iterm2 text-white text-6xl mb-6"></i>
-            <i className="si si-googlemaps text-white text-6xl mb-6"></i>
           </div>
         </div>
       </div>
